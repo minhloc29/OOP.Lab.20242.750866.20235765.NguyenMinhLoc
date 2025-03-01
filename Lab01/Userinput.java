cccb6cc45a5794fea9e52599706f5907cbf68781
macbook <macbook@May-tinh-xach-tay-cua-macbook.local>
2025-03-01 13:50:12 +0700

    Create Lab01 and add the first exercise

---
+++ Lab01/Userinput.java
@@ -0,0 +1,28 @@
+package Lab01;
+import java.util.Scanner;
+
+public class Userinput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("First number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Second number: ");
+        double num2 = scanner.nextDouble();
+
+        double sum = num1 + num2;
+        System.out.println("Sum is: " + sum);
+
+        double diff = num1 - num2;
+        System.out.println("Difference is: " + diff);
+
+        double product = num1 * num2;
+        System.out.println("Product is: " + product);
+
+        double quo = num1 / num2;
+        System.out.println("Quotient is: " + quo);
+
+        scanner.close(); // Close the scanner
+    }
+}
