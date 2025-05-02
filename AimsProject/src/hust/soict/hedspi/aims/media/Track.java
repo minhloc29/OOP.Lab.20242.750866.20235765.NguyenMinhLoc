66965d3835214e9244c8b1725a4b31e9d497fb5d
macbook <macbook@May-tinh-xach-tay-cua-macbook.local>
2025-05-02 14:59:19 +0700

    upload design folder

---
+++ AimsProject/src/hust/soict/hedspi/aims/media/Track.java
@@ -0,0 +1,31 @@
+package hust.soict.hedspi.aims.media;
+
+public class Track implements Playable {
+    private String title;
+    private int length;
+
+    public Track(String title, int length) {
+        this.title = title;
+        this.length = length;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+    public int getLength() {
+        return this.length;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void play(){
+        System.out.println("Playing track: " + this.title);
+        System.out.println("Length: " + this.length);
+    }
+}
---
+++ AimsProject/src/hust/soict/hedspi/test/store/TestStore.java
@@ -0,0 +1,22 @@
+package hust.soict.hedspi.test.store;
+import hust.soict.hedspi.aims.media.DigitalVideoDisc;
+import hust.soict.hedspi.aims.store.Store;
+
+public class TestStore {
+    public static void main(String[] args) {
+        Store ts = new Store();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+                "Animation", "Roger Allers", 87, 19.95f);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
+                "Science Fiction", "George Lucas", 87, 24.95f);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
+                "Animation", 18.99f);
+
+        ts.addDVD(dvd1);
+        ts.addDVD(dvd2);
+        ts.addDVD(dvd3);
+    }
+}
