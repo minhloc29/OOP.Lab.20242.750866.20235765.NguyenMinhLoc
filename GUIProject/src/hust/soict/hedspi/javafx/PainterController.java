1b5a4b176ff2055ba4d40324793e4b63675ad24d
macbook <macbook@May-tinh-xach-tay-cua-macbook.local>
2025-05-23 10:07:36 +0700

    update gui project

--- GUIProject/src/hust/soict/hedspi/javafx/PainterController.java
+++ GUIProject/src/hust/soict/hedspi/javafx/PainterController.java
@@ -1,23 +1,27 @@
 package hust.soict.hedspi.javafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseEvent;
+import javax.swing.*;
 
 public class PainterController {
     @FXML
-    private Pane drawingAreaPane;
+    private VBox drawingAreaPane;
+
+    @FXML
+    void drawingAreaMousePressed(MouseEvent event) {
+        Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
+        drawingAreaPane.getChildren().add(newCircle);
+    }
     @FXML
     void clearButtonPressed(ActionEvent event) {
         drawingAreaPane.getChildren().clear();
     }
-    @FXML
-    void drawingAreaMouseDragged(MouseEvent event) {
-        Circle circle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
-        drawingAreaPane.getChildren().add(circle);
-    }
+
 }
